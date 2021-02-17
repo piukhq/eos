e0748c36dbcd1cbac3ff341f877aa2fb1ca78452
Stewart Perrygrove <sperrygrove@bink.com>
2021-02-17 11:16:11 +0000

    Add static files url pattern

--- app/urls.py
+++ app/urls.py
@@ -13,11 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
+from django.views.static import serve
 from .views import livez
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("livez", view=livez, name="livez"),
+    re_path(r"^static/(?P<path>.*)$", serve, kwargs={"document_root": settings.STATIC_ROOT}),
 ]
